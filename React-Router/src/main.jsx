944b8c8fb2fbaf090508684bc523249949726ed4
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-10-07 06:38:49 -0700

    install router nad use router

--- React-Router/src/main.jsx
+++ React-Router/src/main.jsx
@@ -1,10 +1,59 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+let  Home=()=>{
+  return(
+    <>
+    <h1>I am Home  Page</h1>
+    </>
+  )
+}
+
+let  About=()=>{
+  return(
+    <>
+    <h1>I am About Page</h1>
+    </>
+  )
+}
+let  Profile=()=>{
+  return(
+    <>
+    <h1>I am Profile Page</h1>
+    </>
+  )
+}
+
+let  Data=()=>{
+  return(
+    <>
+    <h1>I am data Page</h1>
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
+
+
+
   <StrictMode>
-    <App />
+ < BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/about' element={<About/>}/>
+  {/* //using nested route */}
+  <Route path='account'>
+
+  <Route path='Profile' element={<Profile/>}/>
+ <Route path='data' element={<Data/>}/>
+
+  </Route>
+</Routes>
+    {/* <App /> */}
+
+ </BrowserRouter>
   </StrictMode>,
 )
