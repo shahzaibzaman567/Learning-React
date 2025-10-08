87d358dd6748b905581d947b33e20403deb0e498
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-10-08 06:37:06 -0700

    call api and get data

--- React-Router/src/main.jsx
+++ React-Router/src/main.jsx
@@ -1,13 +1,36 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { useEffect } from 'react'
 // import App from './App.jsx'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route,useParams,NavLink} from 'react-router-dom'
 
 let  Home=()=>{
+  
+  const [posts,setPost]=useState([]);
+  useEffect(()=>{
+    
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((d)=>d.json())
+    .then((d)=>setPost(d))
+    
+  },[])
+  
+  console.log(posts)
+  
   return(
     <>
     <h1>I am Home  Page</h1>
+    <div style={{padding:40}}>
+     {
+       
+       posts.map((p,i)=>(
+         
+         
+         <NavLink to={`/post/${p.userId}`} style={{display:'block',}} >{p.title}</NavLink>
+        ))
+      }
+    </div>
     </>
   )
 }
@@ -28,29 +51,56 @@ let  Profile=()=>{
 }
 
 let  Data=()=>{
+  //using the hook of params to get name of dynamical
   return(
     <>
-    <h1>I am data Page</h1>
+    <h1>I am account Page </h1>
     </>
   )
 }
 
+let  PostPage=()=>{
+  const param=useParams()
+  console.log("params of nav link or post ",param.userId,param)
+  //using the hook of params to get name of dynamical
+  return(
+    <>
+    <h1>I am postPage </h1>
+    </>
+  )
+}
+
+let User=()=>{
+  const parms=useParams();
+  console.log(parms)
+  return(
+    <>
+  <h1>My name is {parms.userName}</h1>
+
+  </>
+  )
+}
+
+
 createRoot(document.getElementById('root')).render(
-
-
-
+  
   <StrictMode>
  < BrowserRouter>
 <Routes>
   <Route path='/' element={<Home/>}/>
   <Route path='/about' element={<About/>}/>
+  <Route path={`/post/:userId`} element={<PostPage/>}/>
   {/* //using nested route */}
   <Route path='account'>
 
-  <Route path='Profile' element={<Profile/>}/>
+  <Route path='profile' element={<Profile/>}/>
+
  <Route path='data' element={<Data/>}/>
 
   </Route>
+
+<Route path='/user/:userName' element={<User/>}/>
+
 </Routes>
     {/* <App /> */}
 
